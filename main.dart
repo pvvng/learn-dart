0364a7a9fb4f3999a6ec74806a33815139b070e8
맥동우 <t6u80o@naver.com>
2025-01-23 20:05:06 +0900

    late

--- main.dart
+++ main.dart
@@ -1,6 +1,9 @@
 void main() {
-  final name = "kim";
-  // name = "lee";
+  // 재할당 가능한 변수 생성
+  // 자바에도 비슷한게 있었는데..
+  late final String name;
+  // do something...
+  name = "kim";
 
   print("hello world");
 }
