7a5be3cc0c960150c00a4cd85a57c7c618137def
맥동우 <t6u80o@naver.com>
2025-01-23 20:00:58 +0900

    final

--- main.dart
+++ main.dart
@@ -1,16 +1,6 @@
 void main() {
-  // dart 변수는 기본적으로 not nullable
-  // ? => null or string => nullable
-  String? name = "kim";
-  name = null;
-
-  // null check 1
-  if (name != null) {
-    name.length;
-  }
-
-  // null check 2
-  name?.isEmpty;
+  final name = "kim";
+  // name = "lee";
 
   print("hello world");
 }
