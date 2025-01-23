05d519ce61c9d1b98d59b0592868266a155e557e
맥동우 <t6u80o@naver.com>
2025-01-23 20:09:03 +0900

    const

--- main.dart
+++ main.dart
@@ -1,9 +1,7 @@
 void main() {
-  // 재할당 가능한 변수 생성
-  // 자바에도 비슷한게 있었는데..
-  late final String name;
-  // do something...
-  name = "kim";
+  // dart constant => compile-time-constant
+  // js constant = dart fianl
+  const api_key = "keyname";
 
   print("hello world");
 }
