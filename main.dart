6e6cb7fa5735ff2df93b6686150554b0899b0db1
맥동우 <t6u80o@naver.com>
2025-01-23 19:45:00 +0900

    dynamic type

--- main.dart
+++ main.dart
@@ -1,8 +1,13 @@
 void main() {
-  // 지역 변수로 사용
-  var vary = "다트";
-  // 클래스의 프로퍼티로 사용
-  String str = "다트";
-  // 재할당 가능
+  dynamic name;
+
+  if (name is String) {
+    name.contains("kim");
+  }
+
+  if (name is int) {
+    name.isEven;
+  }
+
   print("hello world");
 }
