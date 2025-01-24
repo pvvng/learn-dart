ddd042e8739149971508139776eb0201a302581d
맥동우 <t6u80o@naver.com>
2025-01-24 13:05:12 +0900

    data types

--- main.dart
+++ main.dart
@@ -1,7 +1,12 @@
 void main() {
-  // dart constant => compile-time-constant
-  // js constant = dart fianl
-  const api_key = "keyname";
+  // 거의 모든 자료형은 클래스(오브젝트)
+  // 근데 이건 다른 언어도 마찬가지일듯? 자바나 자스같은것도
+  String str = "name";
+  bool boolean = true;
 
-  print("hello world");
+  num number = 123;
+  number = 12.3;
+  // int, double은 num 클래스타입을 상속받은 클래스임
+  int integer = 123;
+  double doubl3 = 12.3;
 }
