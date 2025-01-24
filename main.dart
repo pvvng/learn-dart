e11e937b1815d23ed3bc66efde87c90d53e0c1c4
맥동우 <t6u80o@naver.com>
2025-01-24 13:12:08 +0900

    list, collection if

--- main.dart
+++ main.dart
@@ -1,12 +1,15 @@
 void main() {
-  // 거의 모든 자료형은 클래스(오브젝트)
-  // 근데 이건 다른 언어도 마찬가지일듯? 자바나 자스같은것도
-  String str = "name";
-  bool boolean = true;
+  var giveMeFive = true;
+  var giveMeSix = false;
+  var list = [
+    1,
+    2,
+    3,
+    4,
+    // collection if
+    if (giveMeFive) 5,
+    if (giveMeSix) 6,
+  ];
 
-  num number = 123;
-  number = 12.3;
-  // int, double은 num 클래스타입을 상속받은 클래스임
-  int integer = 123;
-  double doubl3 = 12.3;
+  print(list);
 }
