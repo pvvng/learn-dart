661489dc4db2b8aaf2f7db78877d43a6e3973766
맥동우 <t6u80o@naver.com>
2025-01-25 23:24:30 +0900

    named parameter

--- main.dart
+++ main.dart
@@ -1,11 +1,27 @@
-String sayHello(String name) {
-  return 'hello $name';
+// use required
+String sayHello({
+  required String name,
+  required int age,
+  required String country,
+}) {
+  return 'hello $name $age, you are from $country';
 }
 
-// arrow function(fat arrow syntax)
-String sayHello2(String name) => 'hello $name';
+// use default value
+String sayHello2({
+  String name = 'user',
+  int age = 99,
+  String country = 'korea',
+}) {
+  return 'hello $name $age, you are from $country';
+}
 
 void main() {
-  print(sayHello('kim'));
-  print(sayHello2('lee'));
+  print(sayHello(
+    name: 'kim',
+    age: 24,
+    country: 'korea',
+  ));
+
+  print(sayHello2());
 }
