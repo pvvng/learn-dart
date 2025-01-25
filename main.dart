ba1f6912e317c2c0a2ef40d515f52f3a68be606f
맥동우 <t6u80o@naver.com>
2025-01-25 23:38:43 +0900

    optional positional parameter

--- main.dart
+++ main.dart
@@ -1,27 +1,7 @@
-// use required
-String sayHello({
-  required String name,
-  required int age,
-  required String country,
-}) {
-  return 'hello $name $age, you are from $country';
-}
-
-// use default value
-String sayHello2({
-  String name = 'user',
-  int age = 99,
-  String country = 'korea',
-}) {
-  return 'hello $name $age, you are from $country';
+String sayHello(String name, int age, [String? country = 'korea']) {
+  return 'hello $name, $age, you are from $country';
 }
 
 void main() {
-  print(sayHello(
-    name: 'kim',
-    age: 24,
-    country: 'korea',
-  ));
-
-  print(sayHello2());
+  print(sayHello('kim', 24));
 }
