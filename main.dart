da52790d21ef96b11337d60fc87498ceaafd7fd3
맥동우 <t6u80o@naver.com>
2025-01-26 15:02:20 +0900

    mixin

--- main.dart
+++ main.dart
@@ -8,9 +8,22 @@ class Human {
   }
 }
 
+// mixin class 내부의 프로퍼티와 메서드를 가져다 쓸 수 있음
+// extends처럼 부모 클래스가 되는 것이 아님
+// mixin 클래스는 생성자가 없어야함.
+mixin Strong {
+  final double strengthLevel = 1500.99;
+}
+
+mixin QuickRunner {
+  void runQuick() {
+    print("ruuuuuuun");
+  }
+}
+
 enum Team { red, blue }
 
-class Player extends Human {
+class Player extends Human with Strong {
   final Team team;
 
   // super 키워드를 통해 부모 클래스와 상호작용 가능
@@ -32,4 +45,6 @@ void main() {
     team: Team.red,
     name: "kim",
   );
+
+  print(player.strengthLevel);
 }
