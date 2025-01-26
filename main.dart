19ff67f03634be000b45462a5f7d59654b669685
맥동우 <t6u80o@naver.com>
2025-01-26 13:43:42 +0900

    constructor

--- main.dart
+++ main.dart
@@ -1,6 +1,9 @@
 class Player {
-  final String name = "dongwoo";
-  int xp = 1500;
+  final String name;
+  int xp;
+
+  // // constructor method
+  Player(this.name, this.xp);
 
   void sayHello() {
     // this 안써도 됨
@@ -9,7 +12,8 @@ class Player {
 }
 
 void main() {
-  var player1 = Player();
-
+  var player1 = Player("kim", 1500);
   player1.sayHello();
+  var player2 = Player("lee", 500);
+  player2.sayHello();
 }
